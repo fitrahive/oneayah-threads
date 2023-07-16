b6c7734efb142546706d4132e6e3dec32756e31e
Abu Masyail <20874779+sooluh@users.noreply.github.com>
2023-07-17 00:00:38 +0700

    feat: bang udah bang

---
+++ src/quran.ts
@@ -0,0 +1,90 @@
+import crypto from 'crypto'
+import * as path from 'path'
+import * as fs from 'fs/promises'
+
+type RandomType = {
+  surah: {
+    id: number
+    arabic: string
+    latin: string
+    transliteration: string
+    translation: string
+    ayah: number
+    mushaf: number
+    location: string
+  }
+  ayah: {
+    id: number
+    surah_id: number
+    ayah: number
+    page: number
+    juz: number
+    manzil: number
+    arabic: string
+    kitabah: string
+    latin: string
+    arabic_words: string[]
+    translation: string
+    footnotes: string | null
+  }
+  tafsir: { title: string; description: string }[]
+}
+
+function getRandomNumber(min: number, max: number) {
+  const range = max - min + 1
+  const buffer = crypto.randomBytes(4)
+  const value = buffer.readUint32LE()
+
+  return (value % range) + min
+}
+
+export async function random(): Promise<RandomType> {
+  const publishedPath = path.resolve(import.meta.dir, '..', 'data/published.json')
+  const isExist = await fs.exists(publishedPath)
+
+  const quranPath = path.resolve(import.meta.dir, '..', 'data/static/surah.json')
+  const quranData = await Bun.file(quranPath).json()
+
+  let surahRand: number = 1
+  let ayahRand: number = 1
+  let newRand: string = '1-1'
+  let published: string[] = []
+
+  if (isExist) {
+    published = await Bun.file(publishedPath).json()
+  }
+
+  if (
+    quranData.reduce((total: number, item: { ayah: number }) => total + item.ayah, 0) ===
+    published.length
+  ) {
+    // reset published if it's full
+    published = ['1-1']
+  }
+
+  if (!Boolean(Bun.env.BISMILLAH) || published.length > 0) {
+    do {
+      surahRand = getRandomNumber(1, quranData.length)
+      ayahRand = getRandomNumber(1, quranData[surahRand - 1].ayah)
+
+      newRand = `${surahRand}-${ayahRand}`
+    } while (published.includes(newRand))
+  }
+
+  published.push(newRand)
+  await Bun.write(publishedPath, JSON.stringify(published, undefined, 2))
+
+  const surahPath = path.resolve(import.meta.dir, '..', 'data/surah', `${surahRand}.json`)
+  const surahData = await Bun.file(surahPath).json()
+
+  const tafsirPath = path.resolve(
+    import.meta.dir,
+    '..',
+    'data/tafsir',
+    surahRand.toString(),
+    `${ayahRand}.json`
+  )
+  const tafsirData = await Bun.file(tafsirPath).json()
+
+  return { surah: quranData[surahRand - 1], ayah: surahData[ayahRand - 1], tafsir: tafsirData }
+}
--- src/threads.ts
+++ src/threads.ts
@@ -1,1 +1,75 @@
-console.log('Hello via Bun!')
+import * as path from 'path'
+import { random } from './quran'
+import * as fs from 'fs/promises'
+import * as Threads from '../modules/threads-api/threads-api/src'
+
+let token: string = ''
+
+const tokenPath = path.resolve(import.meta.dir, '..', 'data/token.json')
+if (await fs.exists(tokenPath)) {
+  token = await Bun.file(tokenPath).json()
+}
+
+const threads = new Threads.ThreadsAPI({
+  verbose: true,
+  token: token || undefined,
+  username: process.env.THREADS_USERNAME,
+  password: process.env.THREADS_PASSWORD,
+})
+token = (await threads.getToken()) || ''
+
+await Bun.write(tokenPath, JSON.stringify(token))
+
+async function publish() {
+  let parentId: string | undefined = ''
+
+  const current = await random()
+  const text =
+    `${current.ayah.arabic}\n\n${current.ayah.translation} ` +
+    `(QS. ${current.surah.transliteration}: ${current.ayah.ayah})`
+
+  parentId = await threads.publish({ text })
+
+  if (text.length <= 500 || !parentId) {
+    publish()
+    return
+  }
+
+  if (current.ayah.footnotes) {
+    const childrenId = await threads.publish({
+      text: `Catatan Kaki:\n\n${current.ayah.footnotes}`,
+      parentPostID: parentId.split('_')[0],
+    })
+
+    if (!childrenId) {
+      console.error('Failed to post footnotes.')
+    }
+
+    parentId = childrenId || parentId
+  }
+
+  const shortTafsir = current.tafsir.filter(
+    (tafsir) => `${tafsir.title}\n\n${tafsir.description}`.length <= 500
+  )
+
+  if (shortTafsir && shortTafsir.length > 0) {
+    for (const tafsir of shortTafsir) {
+      console.log(parentId)
+
+      const childrenId = await threads.publish({
+        text: `${tafsir.title}\n\n${tafsir.description}`,
+        parentPostID: parentId.split('_')[0],
+      })
+
+      if (!childrenId) {
+        console.error('Failed to post tafsir.')
+      }
+
+      parentId = childrenId || parentId
+    }
+  }
+
+  console.log('Done.')
+}
+
+publish()
